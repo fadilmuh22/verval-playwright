7b07b118a52cbc06f9400965c1fb20be34a7b8e7
fadilmuh22 <fadilmuh2002@gmail.com>
2023-11-23 13:37:32 +0700

    feat: created tests on add todo

--- tests/1-add-todo.spec.ts
+++ tests/1-add-todo.spec.ts
@@ -6,13 +6,29 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("should allow me to add todo items", async ({ page }) => {
-  await createTodo(page, TODO_ITEMS.slice(0, 1));
+  await page.getByPlaceholder("What needs to be done?").click();
+  await page.getByPlaceholder("What needs to be done?").fill("ngoding");
+  await page.getByPlaceholder("What needs to be done?").press("Enter");
+
+  await expect(page.getByTestId("todo-count")).toContainText("1");
+  await expect(page.getByTestId("todo-title")).toContainText("ngoding");
 });
 
 test("should clear text input field when an item is added", async ({
   page,
-}) => {});
+}) => {
+  await page.getByPlaceholder("What needs to be done?").click();
+  await page.getByPlaceholder("What needs to be done?").fill("nugas");
+  await page.getByPlaceholder("What needs to be done?").press("Enter");
 
-test("should append new items to the bottom of the list", async ({
-  page,
-}) => {});
+  await expect(page.getByPlaceholder("What needs to be done?")).toBeEmpty();
+});
+
+test("should append new items to the bottom of the list", async ({ page }) => {
+  await createTodo(page, TODO_ITEMS);
+
+  await expect(page.getByTestId("todo-count")).toContainText("3");
+  for (const [index, item] of TODO_ITEMS.entries()) {
+    await expect(page.getByTestId("todo-title").nth(index)).toContainText(item);
+  }
+});
